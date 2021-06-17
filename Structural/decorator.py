3e3b6a99ff3310959a1fb53621444dd09c8ab2bf
Shayan-9248 <shayan.aimoradii@gmail.com>
2021-06-18 03:04:30 +0430

    decorator pattern

---
+++ Structural/decorator.py
@@ -0,0 +1,35 @@
+"""
+    Structural:
+      - Decorator pattern != python pattern
+"""
+
+class Article:
+    def show(self):
+        print('All articles...')
+
+
+class Login:
+    def check_login(self, username, password):
+        if username == 'shayan-9248' and  password == 'max123':
+            return True
+
+
+def outerLogin(func):
+    def innerLogin():
+        username = input('Enter your username: ')
+        password = input('Enter your password: ')
+        l = Login()
+        result = l.check_login(username, password)
+        if result:
+            func()
+        else:
+            print('Wrong username or password')
+    return innerLogin
+
+
+@outerLogin
+def show_all_articles():
+    a = Article()
+    a.show()
+
+show_all_articles()
